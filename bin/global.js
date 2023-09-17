b71925b1b35c30279b2a0a38e9da8a815e2546dc
Rolando Osorio <ragosorio777@gmail.com>
2023-09-17 00:59:23 -0600

    POKERMI V 1.0.0

---
+++ bin/global.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+import searchPokemon from "../src/index.js"
+const text = process.argv[2];
+
+searchPokemon(text)
---
+++ src/index.js
@@ -0,0 +1,42 @@
+import fetch from "node-fetch";
+
+async function searchPokemon(name) {
+  //Empieza la API
+  const url = `https://pokeapi.co/api/v2/pokemon/${name}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    console.log(`
+    Nombre: ${data.forms[0].name}
+    ID: ${data.id}
+    Tipo: ${data.types.map((tipo) => tipo.type.name).join(", ")}
+    ${data.stats[0].stat.name}: ${data.stats[0].base_stat}
+    ${data.stats[1].stat.name}: ${data.stats[1].base_stat}
+    ${data.stats[2].stat.name}: ${data.stats[2].base_stat}
+    ${data.stats[3].stat.name}: ${data.stats[3].base_stat}
+    ${data.stats[4].stat.name}: ${data.stats[4].base_stat}
+    ${data.stats[5].stat.name}: ${data.stats[5].base_stat}
+    Altura: ${data.height}
+    Peso: ${data.weight}
+    Habilidades: ${data.abilities.map((tipo) => tipo.ability.name).join(", ")}
+    Movimientos: ${data.moves
+      .map((movimiento) => movimiento.move.name)
+      .slice(0, 3)
+      .join(", ")}
+    Movimientos+: ${data.moves
+      .map((movimiento) => movimiento.move.name)
+      .slice(4, 7)
+      .join(", ")}
+    Movimientos++: ${data.moves
+      .map((movimiento) => movimiento.move.name)
+      .slice(8, 11)
+      .join(", ")}
+    `);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export default searchPokemon;
