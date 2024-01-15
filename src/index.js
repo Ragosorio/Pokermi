8e9231c281cb34d642d5bab26b8c74b2154d0abb
Rolando Osorio <ragosorio777@gmail.com>
2024-01-14 18:50:30 -0600

    New emojis

--- src/index.js
+++ src/index.js
@@ -12,28 +12,28 @@ async function searchPokemon(name) {
       console.log(`
     Nombre: ${data.forms[0].name}
     ID: ${data.id}
-    Tipo: ${data.types.map((tipo) => tipo.type.name).join(", ")}
-    ${data.stats[0].stat.name}: ${data.stats[0].base_stat}
-    ${data.stats[1].stat.name}: ${data.stats[1].base_stat}
-    ${data.stats[2].stat.name}: ${data.stats[2].base_stat}
-    ${data.stats[3].stat.name}: ${data.stats[3].base_stat}
-    ${data.stats[4].stat.name}: ${data.stats[4].base_stat}
-    ${data.stats[5].stat.name}: ${data.stats[5].base_stat}
-    Altura: ${data.height}
-    Peso: ${data.weight}
-    Habilidades: ${data.abilities.map((tipo) => tipo.ability.name).join(", ")}
+    Tipo: ${data.types.map((tipo) => tipo.type.name).join(", ")} 🧬
+    ${data.stats[0].stat.name}: ${data.stats[0].base_stat} 📊
+    ${data.stats[1].stat.name}: ${data.stats[1].base_stat} 📊
+    ${data.stats[2].stat.name}: ${data.stats[2].base_stat} 📊
+    ${data.stats[3].stat.name}: ${data.stats[3].base_stat} 📊
+    ${data.stats[4].stat.name}: ${data.stats[4].base_stat} 📊
+    ${data.stats[5].stat.name}: ${data.stats[5].base_stat} 📊
+    Altura: ${data.height} 📏
+    Peso: ${data.weight} ⚖️
+    Habilidades: ${data.abilities.map((tipo) => tipo.ability.name).join(", ")} 🎓
     Movimientos: ${data.moves
       .map((movimiento) => movimiento.move.name)
       .slice(0, 3)
-      .join(", ")}
+      .join(", ")} 🔝
     Movimientos+: ${data.moves
       .map((movimiento) => movimiento.move.name)
       .slice(4, 7)
-      .join(", ")}
+      .join(", ")} 🔝
     Movimientos++: ${data.moves
       .map((movimiento) => movimiento.move.name)
       .slice(8, 11)
-      .join(", ")}
+      .join(", ")} 🔝
     `);
     } catch (error) {
       console.log("No se encontraron resultados :(");
